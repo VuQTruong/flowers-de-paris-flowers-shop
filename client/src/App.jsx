dbb6b6b2654fbcfa4bff8dc3b4f33863c0afc703
Vu Truong <q_truong@fanshaweonline.ca>
2022-02-03 11:31:13 -0500

    misc/404 page and client, admin layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoadingPage from './containers/LoadingPage/LoadingPage';
 
-const Home = lazy(() => import('./containers/Home/Home'));
+import ScrollToTop from './components/ScrollToTop/ScropToTop';
+import LoadingPage from './containers/LoadingPage/LoadingPage';
+import ClientLayout from './containers/client/ClientLayout/ClientLayout';
+import AdminLayout from './containers/admin/AdminLayout/AdminLayout';
+
+import Home from './containers/client/Home/Home';
+import Page404 from './containers/Page404/Page404';
+
+// lazy load components
+const Dashboard = lazy(() => import('./containers/admin/Dashboard/Dashboard'));
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Suspense fallback={<LoadingPage />}>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/loading' element={<LoadingPage />} />
+          {/* client routes */}
+          <Route path='/' element={<ClientLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+
+          {/* admin routes */}
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+
+          {/* page not found */}
+          <Route path='*' element={<Page404 />} />
         </Routes>
       </Suspense>
     </Router>
---
+++ client/src/containers/Page404/Page404.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Page404SVG } from '../../assets/undraw_lost_re_xqjt.svg';
+
+function Page404() {
+  return (
+    <div className='container center column'>
+      <Page404SVG className='notfound__svg' />
+      <h2 className='notfound__title'>
+        404 - Looks like you are getting lost!
+      </h2>
+      <Link to='/' className='notfound__back-home'>
+        <i className='bx bx-home'></i>
+        Go back to Home page
+      </Link>
+    </div>
+  );
+}
+
+export default Page404;
