6e27492a7ca44a01567d3dd94c527a20af0fa11d
Vu Truong <q_truong@fanshaweonline.ca>
2022-02-05 21:08:24 -0500

    page404/redo the styles

--- client/src/containers/Page404/Page404.jsx
+++ client/src/containers/Page404/Page404.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Page404SVG } from '../../assets/svgs/undraw_lost_re_xqjt.svg';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 
 function Page404() {
   return (
-    <div className='container flex col center notfound__container'>
-      <Page404SVG className='notfound__svg' />
-      <h2 className='notfound__title'>404 - Looks like you are getting lost!</h2>
-      <Link to='/' className='notfound__back-home'>
-        <i className='bx bx-home'></i>
-        Go back to Home page
-      </Link>
+    <div className='page-container'>
+      <Header />
+      <div className='container flex col center notfound__container'>
+        <Page404SVG className='notfound__svg' />
+        <h2 className='notfound__title'>
+          404 - Looks like you are getting lost!
+        </h2>
+        <Link to='/' className='notfound__back-home'>
+          <i className='bx bx-home'></i>
+          Go back to Home page
+        </Link>
+      </div>
+      <Footer />
     </div>
   );
 }
