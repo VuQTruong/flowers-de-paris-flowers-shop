cf54a394515f60c5c1d9a61c7aa42ea9c28054ea
Vu Truong <q_truong@fanshaweonline.ca>
2022-03-06 23:29:14 -0500

    admin-sidebar/remove unimplemented features

--- client/src/components/Admin/AdSideBar/AdSideBar.jsx
+++ client/src/components/Admin/AdSideBar/AdSideBar.jsx
@@ -24,152 +24,150 @@ function AdSideBar() {
   };
 
   return (
-    <div>
-      <ul className={`admin-sidebar ${showSidebar ? 'active' : ''}`}>
-        <li className='admin-sidebar__item'>
-          <div className='admin-sidebar__toggle' onClick={showSidebarHandler}>
-            <i className='bx bx-chevrons-right'></i>
-          </div>
-          <div className='admin-sidebar__tooltip'>Expand</div>
-        </li>
+    <ul className={`admin-sidebar ${showSidebar ? 'active' : ''}`}>
+      <li className='admin-sidebar__item'>
+        <div className='admin-sidebar__toggle' onClick={showSidebarHandler}>
+          <i className='bx bx-chevrons-right'></i>
+        </div>
+        <div className='admin-sidebar__tooltip'>Expand</div>
+      </li>
 
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/dashboard'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bxs-dashboard'></i>
-            Dashboard
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Dashboard</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/analytic'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-line-chart'></i>
-            Analytic
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Analytic</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/orders'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-spreadsheet'></i>
-            Orders
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Orders</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/categories'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-category'></i>
-            Categories
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Categories</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/products'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-store-alt'></i>
-            Products
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Products</div>
-        </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/dashboard'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bxs-dashboard'></i>
+          Dashboard
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Dashboard</div>
+      </li>
+      {/* <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/analytic'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-line-chart'></i>
+          Analytic
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Analytic</div>
+      </li> */}
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/orders'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-spreadsheet'></i>
+          Orders
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Orders</div>
+      </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/categories'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-category'></i>
+          Categories
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Categories</div>
+      </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/products'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-store-alt'></i>
+          Products
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Products</div>
+      </li>
 
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/tags'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-purchase-tag'></i>
-            Tags
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Tags</div>
-        </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/tags'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-purchase-tag'></i>
+          Tags
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Tags</div>
+      </li>
 
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/accounts'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bxs-user-account'></i>
-            Accounts
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Accounts</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/blogs'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-notepad'></i>
-            Blogs
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Blogs</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/about'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-info-circle'></i>
-            About us
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>About us</div>
-        </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/accounts'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bxs-user-account'></i>
+          Accounts
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Accounts</div>
+      </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/blogs'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-notepad'></i>
+          Blogs
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Blogs</div>
+      </li>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/about'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-info-circle'></i>
+          About us
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>About us</div>
+      </li>
 
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/contacts'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bxs-contact'></i>
-            Contacts
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Contacts</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/hiring'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bx-user-pin'></i>
-            Hiring
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>Hiring</div>
-        </li>
-        <li className='admin-sidebar__item'>
-          <NavLink
-            to='/admin/config'
-            className={generateAdSideBarClasses}
-            onClick={closeSideBarHandler}
-          >
-            <i className='bx bxs-cog'></i>
-            App Config
-          </NavLink>
-          <div className='admin-sidebar__tooltip'>App Config</div>
-        </li>
-      </ul>
-    </div>
+      <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/contacts'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bxs-contact'></i>
+          Contacts
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Contacts</div>
+      </li>
+      {/* <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/hiring'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bx-user-pin'></i>
+          Hiring
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>Hiring</div>
+      </li> */}
+      {/* <li className='admin-sidebar__item'>
+        <NavLink
+          to='/admin/config'
+          className={generateAdSideBarClasses}
+          onClick={closeSideBarHandler}
+        >
+          <i className='bx bxs-cog'></i>
+          App Config
+        </NavLink>
+        <div className='admin-sidebar__tooltip'>App Config</div>
+      </li> */}
+    </ul>
   );
 }
 
