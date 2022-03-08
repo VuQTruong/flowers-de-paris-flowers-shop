ad24f17d5056b5bea3f1c4307448f06de8ba5c26
Vu Truong <q_truong@fanshaweonline.ca>
2022-03-07 20:21:44 -0500

    app/modify morgan package

--- server/src/app.js
+++ server/src/app.js
@@ -42,9 +42,7 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
